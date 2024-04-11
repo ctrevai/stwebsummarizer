a54f7af82407466c67d86f61c2b84a08ccef1991
ctrevai <ctrevai@gmail.com>
2024-04-11 13:11:47 +0000

    final commit

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ url = st.text_input("Enter a url")
 
 note = st.text_input("Enter a note")
 
-#url = 'https://aws.amazon.com/blogs/media/video-summarization-with-aws-artificial-intelligence-ai-and-machine-learning-ml-services/'
-
-# URL parser
-if url:
-    text = html_loader.html_to_text(url)
+#url = 'https://aws.amazon.com/blogs/media/video-summarization-with-aws-artificial-intelligence-ai-and-machine-learning-ml-services/'    
 
 # Core system prompt
 if url and note:
+    #parse URL
+    text = html_loader.html_to_text(url)
+    #prepare prompt
     prompt = '''
 
     Provide the summary of the following text
